ac111c9e93f94d5e89d13395d36b2d9f32e1f61b
zarubind <64904847+zarubind@users.noreply.github.com>
2021-09-27 00:34:22 +0700

    Add files via upload

---
+++ internet_parsing/lesson1/lesson1.py
@@ -0,0 +1,21 @@
+# 1. Посмотреть документацию к API GitHub, разобраться как вывести список репозиториев для конкретного пользователя,
+# сохранить JSON-вывод в файле *.json.
+
+import os
+import requests
+
+repos = requests.get('https://api.github.com/users/zarubind/repos?per_page=1000')
+for i in repos.json():
+    print(i['name'])
+#print(repos.json())
+with open('1.txt', 'w') as file:
+    file.write(repos.text)
+
+# 2. Изучить список открытых API. Найти среди них любое, требующее авторизацию (любого типа).
+# Выполнить запросы к нему, пройдя авторизацию. Ответ сервера записать в файл.
+
+username = 'zarubind'
+token = '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'
+repos = requests.get('https://api.github.com/user', auth=(username, token))
+with open('2.txt', 'w') as file:
+    file.write(repos.text)
